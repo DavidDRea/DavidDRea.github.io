e8535c9d25ee1b157606ad92672338292fe4459f
DavidDRea <programming.david@gmail.com>
2018-06-09 23:57:49 +0100

    adding type effect

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+let timer1 = 0;
+let timer2 = 0;
+const speed = 250;
+const name = "David Rea";
+const job = "Front End Developer"
+
+const typing = (text, text2) => {
+	if(timer1 < text.length) {
+		const textNode = document.createTextNode(text.charAt(timer1));
+		document.querySelector(".animation1").append(textNode);
+		timer1++;
+		setTimeout(function() {typing(name, job);}, speed);
+	} 
+	if(timer1 === text.length) {
+		if(timer2 < text2.length) {
+			const textNode = document.createTextNode(text2.charAt(timer2));
+			document.querySelector(".animation2").append(textNode);
+			timer2++;
+			setTimeout(function() {typing(name, job);}, speed);
+		} 
+	}
+}
+
+//Scroll banner colour change function
 const changeNavColours = () => {
 	let bgColourEle = document.querySelector(".nav-container");
 	if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
@@ -7,4 +31,5 @@ const changeNavColours = () => {
 	}
 }
 
+window.addEventListener("load", typing(name, job));
 window.onscroll = () => changeNavColours();
