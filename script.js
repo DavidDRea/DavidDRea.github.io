eb918091e098637b21797130cd54fa0285de5573
DavidDRea <programming.david@gmail.com>
2018-07-17 13:33:27 +0100

    Style change

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 // Variables for typing animation
-let timer1 = 0;
-let timer2 = 0;
-const speed = 220;
-const name = "David Rea";
-const job = "Frontend Developer";
+let timer = 0;
+const text = `
+ <html>
+    <head>
+	 <title></title>
+    </head>
+    <body>
+         <h1>David Rea</h1>
+         <h2>Web Developer</h2>
+    </body>
+ </html>`;
+
+//Variables for screen flicker 
+let flickerSpeed = 60;
+const flicker = document.querySelector(".glow");
 
 // Constants for nav button functionality 
 const home = document.querySelector("#home");
@@ -17,39 +27,56 @@ const aboutButton = document.querySelector("#about-button");
  
 const contactButton = document.querySelector("#contact-button");
 
+//Scoll to element function for buttons
 const scrollToElement = (element) => {
 	element.scrollIntoView({behavior: "smooth"});
 }
 
-// Typing animation function
-const typing = (text, text2) => {
-	if(timer1 < text.length) {
-		const textNode = document.createTextNode(text.charAt(timer1));
-		document.querySelector(".animation1").append(textNode);
-		timer1++;
-		setTimeout(function() {typing(name, job);}, speed);
-	} 
-	if(timer1 === text.length) {
-		if(timer2 < text2.length) {
-			const textNode = document.createTextNode(text2.charAt(timer2));
-			document.querySelector(".animation2").append(textNode);
-			timer2++;
-			setTimeout(function() {typing(name, job);}, speed);
-		} 
-	}
-}
+// Typing animation functions
 
-//Scroll banner colour change function
-const changeNavColours = () => {
-	let bgColourEle = document.querySelector(".nav-container");
-	if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
-		bgColourEle.classList.add("change-bg-colour");
+const typing = (text) => {
+	if(timer < text.length) {
+		document.querySelector(".animation1").textContent += text.charAt(timer);
+		timer++;
+		setTimeout(function() {typing(text);}, 80);
 	} else {
-	bgColourEle.classList.remove("change-bg-colour");
+		setTimeout(deleting, 1000);
 	}
 }
 
-window.addEventListener("load", typing(name, job));
+const deleting = () => {
+	if(timer >= 0) {
+		text2 = document.querySelector(".animation1").textContent; 
+		document.querySelector(".animation1").textContent = text2.substr(0, timer);
+		timer--;
+		setTimeout(deleting, 40);
+	} else {
+		setTimeout(function() {typing(text);}, 2000);
+	}
+}
+
+//Randomize flicker function
+const random = () => Math.floor(Math.random() * (180 - 80 + 1)) + 80;
+
+//Screen flicker function
+const flickerAnimation = () => {
+	flicker.classList.toggle("glow-off");
+	flickerSpeed = random();
+	setTimeout(flickerAnimation, flickerSpeed);
+}
+
+// //Scroll banner colour change function
+// const changeNavColours = () => {
+// 	let bgColourEle = document.querySelector(".nav-container");
+// 	if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+// 		bgColourEle.classList.add("change-bg-colour");
+// 	} else {
+// 	bgColourEle.classList.remove("change-bg-colour");
+// 	}
+// }
+
+window.addEventListener("load", typing(text));
+window.addEventListener("load", flickerAnimation());
 window.onscroll = () => changeNavColours();
 projectsButton.onclick = () => scrollToElement(projects);
 homeButton.onclick = () => scrollToElement(home);
